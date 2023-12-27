3dbcff6d7c53731a1ceb680e1db96682c9428ae6
KarmaNamgay <132356064+KarmaNamgay@users.noreply.github.com>
2023-12-27 10:14:12 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function intro_page()
+{
+    window.location(index.html)
+}
+
+function causes_page()
+{
+    windown.location(causes.html)
+}
+
+function impact_page()
+{
+    window,location(impact.html)
+}
+
+function conclusion_page()
+{
+    window.location(conclusion.html)
+}
